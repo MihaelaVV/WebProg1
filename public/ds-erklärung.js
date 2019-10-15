fdd64f5114e1648f022af0eef7fdf55be1e16d88
MihaelaVV <55392093+MihaelaVV@users.noreply.github.com>
2019-10-15 15:41:35 +0200

    Add files via upload

---
+++ public/ds-erklärung.js
@@ -0,0 +1,8 @@
+document.getElementById('button').onclick = function () {
+    window.location.href = 'ticketAnlegen.html'
+}
+
+
+document.getElementById('btnAbbrechen').onclick = function () {
+    alert('Bitte akzeptieren Sie die Datenschutzerklärung.')
+}
