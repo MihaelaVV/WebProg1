0b9c3cb16c82c9f7cc79e179a9f535b33f556b0a
MihaelaVV <55392093+MihaelaVV@users.noreply.github.com>
2019-10-15 15:45:52 +0200

    Delete ds-erklärung.js

--- ds-erklärung.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('button').onclick = function () {
-    window.location.href = 'ticketAnlegen.html'
-}
-
-
-document.getElementById('btnAbbrechen').onclick = function () {
-    alert('Bitte akzeptieren Sie die Datenschutzerklärung.')
-}
